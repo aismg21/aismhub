de5617cfeafdd1d8e813b568529491b2db951727
aismg21 <riyapacker@gmail.com>
2025-11-27 16:25:13 +0530

    udate

--- firebase/config.ts
+++ firebase/config.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTd0fopHZNsBn7YhbEm7_xfBlWXSU5Kgo",
-  authDomain: "aismg-2e343.firebaseapp.com",
-  projectId: "aismg-2e343",
-  storageBucket: "aismg-2e343.appspot.com",
-  messagingSenderId: "796368183141",
-  appId: "1:796368183141:web:a2bad920fcc767d8f78b8c",
+apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // For debugging locally only
